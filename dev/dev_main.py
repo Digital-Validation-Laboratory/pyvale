01d12810e50150dc590723a297178280cff37ecb
ScepticalRabbit <thescepticalrabbit@gmail.com>
2024-02-18 17:12:01 +0000

    Created dev script to read in exodus file from thermal run.

--- dev/dev_main.py
+++ dev/dev_main.py
@@ -5,3 +5,16 @@ pycave: dev_main
 authors: thescepticalrabbit
 -------------------------------------------------------------------------------
 '''
+from pprint import pprint
+from pathlib import Path
+from mooseherder.exodusreader import ExodusReader
+
+def main() -> None:
+    data_path = Path('scripts/moose_thermal_basic_out.e')
+    data_reader = ExodusReader(data_path)
+    sim_data = data_reader.read_all_sim_data()
+
+
+if __name__ == '__main__':
+    main()
+
--- dev/dev_run_moose_thermal.py
+++ dev/dev_run_moose_thermal.py
@@ -11,14 +11,20 @@ from mooseherder.mooserunner import MooseRunner
 
 USER_DIR = Path.home()
 
-config = {'main_path': USER_DIR / 'moose',
-          'app_path': USER_DIR / 'moose-workdir/proteus',
-          'app_name': 'proteus-opt'}
+def main() -> None:
+    config = {'main_path': USER_DIR / 'moose',
+            'app_path': USER_DIR / 'moose-workdir/proteus',
+            'app_name': 'proteus-opt'}
 
-moose_config = MooseConfig(config)
-moose_runner = MooseRunner(moose_config)
+    moose_config = MooseConfig(config)
+    moose_runner = MooseRunner(moose_config)
 
-moose_runner.set_run_opts(n_tasks = 1,n_threads = 4,redirect_out = False)
+    moose_runner.set_run_opts(n_tasks = 1, n_threads = 4, redirect_out = False)
 
-input_path = Path('scripts/moose_thermal_basic.i')
-moose_runner.run(input_path)
+    input_path = Path('scripts/moose_thermal_volumetric.i')
+    moose_runner.run(input_path)
+
+if __name__ == '__main__':
+    main()
+
+
