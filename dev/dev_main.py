3d9fe2858a97489a518d89db672cd38c691f7df4
ScepticalRabbit <thescepticalrabbit@gmail.com>
2024-02-24 12:44:44 +0000

    Displaying sensors on monoblock

--- dev/dev_main.py
+++ dev/dev_main.py
@@ -13,7 +13,8 @@ import pyvista as pv
 from pyvista import CellType
 
 #-------------------------------------------------------------------------------
-def convert_simdata_to_pyvista(sim_data: mh.SimData,) -> pv.UnstructuredGrid:
+def convert_simdata_to_pyvista(sim_data: mh.SimData,dim: int = 3
+                               ) -> pv.UnstructuredGrid:
     flat_connect = np.array([],dtype=np.int64)
     cell_types = np.array([],dtype=np.int64)
 
@@ -26,7 +27,7 @@ def convert_simdata_to_pyvista(sim_data: mh.SimData,) -> pv.UnstructuredGrid:
         idxs = np.arange(0,n_elems*nodes_per_elem,nodes_per_elem,dtype=np.int64)
         temp_connect = np.insert(temp_connect,idxs,nodes_per_elem)
 
-        this_cell_type = get_cell_type(nodes_per_elem,dim=2)
+        this_cell_type = get_cell_type(nodes_per_elem,dim=dim)
         cell_types = np.hstack((cell_types,np.full(n_elems,this_cell_type)))
         flat_connect = np.hstack((flat_connect,temp_connect),dtype=np.int64)
 
@@ -36,7 +37,7 @@ def convert_simdata_to_pyvista(sim_data: mh.SimData,) -> pv.UnstructuredGrid:
     grid = pv.UnstructuredGrid(cells, cell_types, points)
 
     for nn in sim_data.node_vars:
-        grid[nn] = sim_data.node_vars[nn]
+        grid[nn] = sim_data.node_vars[nn]#[:,-1]
 
     return grid
 
@@ -62,8 +63,6 @@ class Field:
     def __init__(self) -> None:
         pass
 
-
-
 #-------------------------------------------------------------------------------
 class Sensor:
     def __init__(self, position: np.ndarray) -> None:
@@ -72,7 +71,7 @@ class Sensor:
     def get_position(self) -> np.ndarray:
         return self._position
 
-    def get_measurement(self,field) -> np.ndarray:
+    def get_measurement(self) -> np.ndarray:
         return self.get_truth() + \
             self.get_systematic_err() + \
             self.get_random_err()
@@ -91,38 +90,10 @@ def build_sensor(position: np.ndarray) -> np.ndarray:
     return np.array((Sensor(position)))
 
 #-------------------------------------------------------------------------------
-def main() -> None:
-    data_path = Path('data/monoblock_thermal_out.e')
-    data_path = Path('data/moose_2d_thermal_basic_out.e')
-
-    data_reader = mh.ExodusReader(data_path)
-    sim_data = data_reader.read_all_sim_data()
-
-    pv_simdata = convert_simdata_to_pyvista(sim_data)
-
-    # Create sensors
-    x_sens = 3
-    x_min = 0
-    x_max = 2
-    y_sens = 2
-    y_min = 0
-    y_max = 1
-
-    sens_pos_x = np.linspace(x_min,x_max,x_sens+2)[1:-1]
-    sens_pos_y = np.linspace(y_min,y_max,y_sens+2)[1:-1]
-    (sens_grid_x,sens_grid_y) = np.meshgrid(sens_pos_x,sens_pos_y)
-
-    sens_pos_x = sens_grid_x.flatten()
-    sens_pos_y = sens_grid_y.flatten()
-    sens_pos_z = np.zeros(sens_pos_x.shape)
-    sens_pos = np.vstack((sens_pos_x,sens_pos_y,sens_pos_z)).T
-
-    sensor_array = np.apply_along_axis(build_sensor,1,sens_pos)
-
-    pv_sensdata = pv.PolyData(sens_pos)
-
+def plot_sensors(pv_simdata: pv.UnstructuredGrid,
+                 pv_sensdata: pv.PolyData) -> None:
     pv.set_plot_theme('dark')
-    pv_plot = pv.Plotter(window_size=[1000, 1000])
+    pv_plot = pv.Plotter(window_size=[1000, 1000]) # type: ignore
     pv_plot.add_mesh(pv_sensdata,
                      label='sensors',
                      color='red',
@@ -131,11 +102,60 @@ def main() -> None:
                      )
 
     pv_plot.add_mesh(pv_simdata,
-                     scalars=pv_simdata['T'][:,-1],
+                     scalars=pv_simdata['temperature'][:,-1],
                      label='sim data',
                      show_edges=True)
-    pv_plot.camera_position = 'xy'
+    #pv_plot.camera_position = 'zy'
+    pv_plot.add_axes_at_origin(labels_off=True)
+    pv_plot.set_scale(xscale = 100, yscale = 100, zscale = 100)
     pv_plot.show()
+
+#-------------------------------------------------------------------------------
+def main() -> None:
+    data_path = Path('data/monoblock_thermal_out.e')
+    #data_path = Path('data/moose_2d_thermal_basic_out.e')
+
+    data_reader = mh.ExodusReader(data_path)
+    sim_data = data_reader.read_all_sim_data()
+
+    pv_simdata = convert_simdata_to_pyvista(sim_data,dim=3)
+
+    x_sens = 1
+    x_min = 11.5e-3
+    x_max = 11.5e-3
+
+    y_sens = 3
+    y_min = -11.5e-3
+    y_max = 19.5e-3
+
+    z_sens = 2
+    z_min = 0
+    z_max = 12e-3
+
+    sens_pos_x = np.linspace(x_min,x_max,x_sens+2)[1:-1]
+    sens_pos_y = np.linspace(y_min,y_max,y_sens+2)[1:-1]
+    sens_pos_z = np.linspace(z_min,z_max,z_sens+2)[1:-1]
+    (sens_grid_x,sens_grid_y,sens_grid_z) = np.meshgrid(
+        sens_pos_x,sens_pos_y,sens_pos_z)
+
+    sens_pos_x = sens_grid_x.flatten()
+    sens_pos_y = sens_grid_y.flatten()
+    sens_pos_z = sens_grid_z.flatten()
+    sens_pos = np.vstack((sens_pos_x,sens_pos_y,sens_pos_z)).T
+
+    sensor_array = np.apply_along_axis(build_sensor,1,sens_pos)
+
+    pv_sensdata = pv.PolyData(sens_pos)
+
+    sens_vals = pv_sensdata.sample(pv_simdata)
+
+    pprint(sens_vals)
+    for nn in sim_data.node_vars:
+        pprint(sens_vals[nn])
+
+    plot_sensors(pv_simdata,pv_sensdata)
+
+
 
 #-------------------------------------------------------------------------------
 if __name__ == '__main__':
--- dev/dev_notes.md
+++ dev/dev_notes.md
@@ -3,5 +3,89 @@
 ## Notes: architecture
 
 ## Notes: numpy
+Avoiding loops by applying a function along an array dimension:
 
 ## Notes: pyvista
+
+### Mesh structure in pyvista
+NOTE: pyvista element numbers are 0 indexed so need to -1 from whole element connectivity table to correct it.
+
+The most general mesh structure in `pyvista` is the `pyvista.UnstructuredGrid`. This requires 3 types of information to construct. Here we assume we have a mesh with `N` nodes and `E` elements with `n_per_e` nodes per element which:
+ 1. A flattened connectivity array (`cells`) of the form `[n_per_e,n_1,n_2,n_3,...]` repeated for each element.
+ 2. A flattened array that specifies the element types (`cell_types`) as an array of length `E` with the `py.CellType` as the value for each element.
+ 3. An array specifying the nodal coordinates as an `Nx3` array of nodal coordinates.
+
+Example multi element mesh:
+```python
+import numpy as np
+import pyvista as pv
+from pyvista import CellType
+
+pv.set_plot_theme("document")
+
+# Define points for the hexahedron and tetrahedron
+points = np.array(
+    [
+        [0.0, 0.0, 0.0],  # 0
+        [1.0, 0.0, 0.0],  # 1
+        [1.0, 1.0, 0.0],  # 2
+        [0.0, 1.0, 0.0],  # 3
+        [0.0, 0.0, 1.0],  # 4
+        [1.0, 0.0, 1.0],  # 5
+        [1.0, 1.0, 1.0],  # 6
+        [0.0, 1.0, 1.0],  # 7
+        [0.75, 0.5, 1.25],  # 8
+    ],
+    dtype=float,
+)
+
+# Define hexahedral cell connectivity
+hex_cell = np.array([8, 0, 1, 2, 3, 4, 5, 6, 7], np.int64)
+
+# Define tetrahedral cell connectivity
+tet_cell = np.array([4, 4, 5, 6, 8], np.int64)
+
+# Combine cell arrays
+cells = np.hstack((hex_cell, tet_cell))
+
+# Define cell types
+cell_types = np.array([CellType.HEXAHEDRON, CellType.TETRA])
+print(cell_types)
+
+# Create unstructured grid
+grid = pv.UnstructuredGrid(cells, cell_types, points)
+
+# Visualize the unstructured grid
+pl = pv.Plotter()
+pl.add_mesh(grid, show_edges=True)
+pl.show_axes()
+pl.show()
+```
+
+### Plotting in pyvista
+Adding multiple meshes to one plot:
+```python
+import pyvista as pv
+
+pv.set_plot_theme('dark')
+pv_plot = pv.Plotter(window_size=[1000, 1000]) # type: ignore
+pv_plot.add_mesh(pv_sensdata,
+                    label='sensors',
+                    color='red',
+                    render_points_as_spheres=True,
+                    point_size=20
+                    )
+
+pv_plot.add_mesh(pv_simdata,
+                    scalars=pv_simdata['T'][:,-1],
+                    label='sim data',
+                    show_edges=True)
+pv_plot.camera_position = 'xy'
+pv_plot.show()
+```
+
+### Interpolating using an FE mesh
+Syntax is:
+```python
+sampled_values = points_to_sample.sample(fe_field_data_to_sample)
+```
