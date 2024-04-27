a907bae03f3bf3b2ee860cb366e6e8b846e04959
ScepticalRabbit <thescepticalrabbit@gmail.com>
2024-04-27 15:53:09 +0100

    Working pymoo optimisation example

--- dev/lf-dev/ex_pymoo_opt.py
+++ dev/lf-dev/ex_pymoo_opt.py
@@ -1,84 +1,68 @@
 # EXAMPLE: pymoo optimisation
 import numpy as np
+import matplotlib.pyplot as plt
+from pycave.plotprops import PlotProps
+
 from pymoo.core.problem import Problem
 from pymoo.algorithms.soo.nonconvex.pso import PSO
+from pymoo.algorithms.soo.nonconvex.ga import GA
 import pymoo.gradient.toolbox as anp
-from pymoo.core.problem import Problem
+from pymoo.termination import get_termination
+from pymoo.termination.default import DefaultSingleObjectiveTermination
 from pymoo.optimize import minimize
+
+from pymoo.problems.single import Rastrigin, Ackley
 
 import pycave.optimisers.checkfuncs as cf
 
 #-------------------------------------------------------------------------------
-class SphereWithConstraint(Problem):
-    def __init__(self):
-        super().__init__(n_var=10, n_obj=1, n_ieq_constr=1, xl=0.0, xu=1.0)
-
-    def _evaluate(self, x, out, *args, **kwargs):
-        out["F"] = np.sum((x - 0.5) ** 2, axis=1)
-        out["G"] = 0.1 - out["F"]
-
-
-class Rastrigin(Problem):
-    def __init__(self, n_var=2, A=10.0):
-        super().__init__(n_var=n_var, n_obj=1, xl=-5, xu=5, vtype=float)
-        self.A = A
-
-    def _evaluate(self, x, out, *args, **kwargs):
-        z = anp.power(x, 2) - self.A * anp.cos(2 * anp.pi * x)
-        out["F"] = self.A * self.n_var + anp.sum(z, axis=1)
-        print(x)
-        print(out["F"])
-
-    def _calc_pareto_front(self):
-        return 0.0
-
-    def _calc_pareto_set(self):
-        return np.full(self.n_var, 0)
-
+def cost_func(x):
+    return cf.rastrigin(x)
 
 class TestProblem(Problem):
-    def __init__(self, n_var=2, A=10.0):
-        super().__init__(n_var=n_var, n_obj=1, xl=-5, xu=5, vtype=float)
-        self.A = A
+    def __init__(self, n_var=2, xl=-10.0, xu=10.0):
+        super().__init__(n_var=n_var, n_obj=1, xl=xl, xu=xu, vtype=float)
 
     def _evaluate(self, x, out, *args, **kwargs):
-        z = anp.power(x, 2) - self.A * anp.cos(2 * anp.pi * x)
-        out["F"] = self.A * self.n_var + anp.sum(z, axis=1)
-
+        out["F"] = cost_func(x)
 
 #-------------------------------------------------------------------------------
 def main() -> None:
-    x = cf.get_flat_x_2d((-5.12,5.12),
-                        (-5.12,5.12),
-                        10)
-    f = cf.rastrigin(x)
-    (fig,ax) = cf.plot_fun_2d('Rastrigin',
-                    cf.rastrigin,
-                    (-5.12,5.12),
-                    (-5.12,5.12),
-                    100)
+    xl = -10.0
+    xu = 10.0
+    problem = TestProblem(xl=xl,xu=xu)
 
-    #print(f)
+    alg = 'GA'
+    if alg == 'PSO':
+        algorithm = PSO()
+    else:
+        algorithm = GA(
+            pop_size=100,
+            eliminate_duplicates=True)
 
-    return
-
-    problem = Rastrigin()
-
-    algorithm = PSO()
+    termination = DefaultSingleObjectiveTermination(
+        xtol=1e-8,
+        cvtol=1e-6,
+        ftol=1e-6,
+        period=10, # Number of generations that need to be below tols
+        n_max_gen=100,
+        n_max_evals=100000)
 
     res = minimize(problem,
                 algorithm,
-                seed=1,
+                termination,
+                #seed=1,
                 verbose=True)
 
+    print(80*'=')
     print("Best solution found: \nX = %s\nF = %s" % (res.X, res.F))
+    print(80*'=')
 
-    return
-    (fig,ax) = cf.plot_fun_2d('Rastrigin',
-                    cf.rastrigin_n,
-                    (-5.12,5.12),
-                    (-5.12,5.12),
-                    100)
+    pp = PlotProps()
+    (fig,ax) = cf.plot_fun_2d(f'Min with {alg}',
+                              cost_func,(xl,xu),(xl,xu),100)
+    plt.plot(res.X[0],res.X[1],'+r',lw=pp.lw,ms=pp.ms)
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- src/pycave/optimisers/checkfuncs.py
+++ src/pycave/optimisers/checkfuncs.py
@@ -10,149 +10,86 @@ import matplotlib.pyplot as plt
 from pycave.plotprops import PlotProps
 
 
-def ackley_n(x: np.ndarray,
+def ackley(x: np.ndarray,
              a: float = 20.0,
              b: float = 0.2,
-             c: float = 2*np.pi) -> float:
+             c: float = 2*np.pi) -> np.ndarray:
     """ACKLEY
     Dimension: N
     Local Minima: many
     Global Minimum: f(x) = 0 @ (0,0,....,0)
     Eval: [-32.768,32.768] or smaller
-
-    Args:
-        x (np.ndarray):
-        a (float, optional): _description_. Defaults to 20.0.
-        b (float, optional): _description_. Defaults to 0.2.
-        c (float, optional): _description_. Defaults to 2*np.pi.
-
-    Returns:
-        float: _description_
     """
-    n = len(x)
-    sum1 = sum(x**2)
-    sum2 = sum(np.cos(c*x))
+    n = x.shape[1]
+    sum1 = np.sum(x**2,axis=1)
+    sum2 = np.sum(np.cos(c*x),axis=1)
     f =  -a*np.exp(-b*np.sqrt(sum1/n)) - np.exp(sum2/n) + a + np.exp(1)
     return f
 
 
-def dixonprice_n(x: np.ndarray) -> float:
+def dixonprice(x: np.ndarray) -> np.ndarray:
     """DIXON-PRICE
     Dimension: N
     Local Minima: Large valley
     Global Minimum: f(x) = 0 @ x_i = 2^-((2^i-2)/2^i) for i = 1,...,d
     Eval: [-10.0,10.0] or smaller
-
-    Args:
-        x (np.ndarray): _description_
-
-    Returns:
-        float: _description_
     """
-    n = len(x)
+    n = x.shape[1]
     j = np.arange( 2, n+1 )
     x2 = 2 * x**2
-    f = sum( j * (x2[1:] - x[:-1]) **2 ) + (x[0] - 1) **2
+    f = np.sum( j * (x2[:,1:] - x[:,:-1]) **2, axis=1) + (x[:,0] - 1) **2
     return f
 
-'''
-def griewank_n(x: np.ndarray, div: float = 4000.0) -> float:
+
+def griewank(x: np.ndarray, div: float = 4000.0) -> np.ndarray:
     """GRIEWANK
     Dimension: N
     Local Minima: many
     Global Minimum: f(x) = 0 @ (0,0,....,0)
     Eval: [-600,600] or smaller
-
-    Args:
-        x (np.ndarray): _description_
-        div (float, optional): _description_. Defaults to 4000.0.
-
-    Returns:
-        float: _description_
     """
-    n = len(x)
+    n = x.shape[1]
     j = np.arange( 1., n+1 )
-    sum1 = sum( x**2 )
-    p = np.prod( np.cos( x / np.sqrt(j) ))
+    sum1 = np.sum( x**2, axis=1 )
+    p = np.prod( np.cos( x / np.sqrt(j) ), axis=1)
     f = sum1/div - p + 1
     return f
-'''
 
-def rastrigin(x: np.ndarray, a: float = 10):
+
+def rastrigin(x: np.ndarray, a: float = 10) -> np.ndarray:
     """RASTRIGIN
     Dimension: N
     Local Minima: many
     Global Minimum: f(x) = 0 @ (0,0,....,0)
     Eval: [-5.12,5.12] or smaller
-
-    Args:
-        x (np.ndarray): _description_
-        a (float, optional): _description_. Defaults to 10.
-
-    Returns:
-        float: _description_
     """
     n = x.shape[1]
     sum1 = np.sum(x**2 - a*np.cos(2*np.pi*x),axis=1)
-    print(sum1.shape)
     f = a*n + sum1
     return f
 
 
-def rastrigin_n(x: np.ndarray, a: float = 10) -> float:
-    """RASTRIGIN
-    Dimension: N
-    Local Minima: many
-    Global Minimum: f(x) = 0 @ (0,0,....,0)
-    Eval: [-5.12,5.12] or smaller
-
-    Args:
-        x (np.ndarray): _description_
-        a (float, optional): _description_. Defaults to 10.
-
-    Returns:
-        float: _description_
-    """
-    n = len(x)
-    sum1 = sum(x**2 - a*np.cos(2*np.pi*x))
-    f = a*n + sum1
-    return f
-
-
-def rosenbrock_n(x: np.ndarray, a: float = 100) -> float:
+def rosenbrock(x: np.ndarray, a: float = 100) -> float:
     """ROSENBROCK
     Dimension: N
     Local Minima: Large valley
     Global minimum: at (x,y) = (1,1) where f(x,y)=0
     Eval: [-5.0,10.0] or smaller
-
-    Args:
-        x (np.ndarray): _description_
-        a (float, optional): _description_. Defaults to 100.
-
-    Returns:
-        _type_: _description_
     """
-    x0 = x[:-1] # x_(i) ... to n-1
-    x1 = x[1:]  # x_(i+1) ... to n-1
-    f = sum(a*(x1-x0**2)**2 + (1-x0)**2)
+    x0 = x[:,:-1] # x_(i) ... to n-1
+    x1 = x[:,1:]  # x_(i+1) ... to n-1
+    f = a*np.sum((x1-x0**2)**2,axis=1) + np.sum((1-x0)**2, axis=1)
     return f
 
 
-def sphere_n(x: np.ndarray) -> float:
+def sphere(x: np.ndarray) -> np.ndarray:
     """SPHERE
     Dimension: N
     Local Minima: none
     Global minimum: f(x) = 0 @ (0,0,....,0)
     Eval: [-inf,inf] or smaller
-
-    Args:
-        x (np.ndarray): _description_
-
-    Returns:
-        float: _description_
     """
-    f = sum(x**2)
+    f = np.sum(x**2,axis=1)
     return f
 
 
@@ -207,8 +144,8 @@ def plot_fun_2d(tStr: str,
     plt.title(tStr,fontsize=pp.font_head_size,fontname=pp.font_name)
     plt.xlabel("x1",fontsize=pp.font_ax_size,fontname=pp.font_name)
     plt.ylabel("x2",fontsize=pp.font_ax_size,fontname=pp.font_name)
-    plt.show()
 
+    #plt.show()
     #plt.savefig(save_path+save_name, dpi=pp.resolution, format='png', bbox_inches='tight')
     return fig,ax
 
