4dd6caf06a8e7f8c5c3a4113210079017b2b8f17
ScepticalRabbit <thescepticalrabbit@gmail.com>
2024-05-17 09:54:49 +0100

    /home/lloydf/miniforge3/envs/lfdev/bin/python /home/lloydf/py-workdir/pyvale/examples/ex1_2d_thermcouples.py

--- simcases/run_one_case.py
+++ simcases/run_one_case.py
@@ -14,7 +14,7 @@ from mooseherder import (MooseConfig,
 
 #======================================
 # Change this to run a different case
-CASE_STR = 'case11'
+CASE_STR = 'case16'
 #======================================
 
 CASE_FILES = (CASE_STR+'.geo',CASE_STR+'.i')
@@ -46,7 +46,7 @@ def main() -> None:
     moose_runner = MooseRunner(moose_config)
 
     moose_runner.set_run_opts(n_tasks = 1,
-                              n_threads = 7,
+                              n_threads = 8,
                               redirect_out = False)
 
     moose_start_time = time.perf_counter()
