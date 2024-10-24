b7567a71cb6d25b506fd153784baad8639580c5b
ScepticalRabbit <thescepticalrabbit@gmail.com>
2024-10-24 15:59:25 +0100

    Working on calibration errors

--- dev/lfdev/dev_calib_check.py
+++ dev/lfdev/dev_calib_check.py
@@ -9,6 +9,11 @@ Copyright (C) 2024 The Digital Validation Team
 '''
 import numpy as np
 
+# Known: what the temperature should be - truth
+# 1) Calculate the voltage using the true calibration
+# 2) Use this voltage to calculate the assumed calibrated temp
+# 3) Error = assumed - truth
+
 def assumed_calib(vals: np.ndarray) -> np.ndarray:
     return np.array([])
 
