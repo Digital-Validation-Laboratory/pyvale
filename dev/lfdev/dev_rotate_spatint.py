Halfway through fixing rotation of integration points in spatial integrator

dev/lfdev/dev_rotate_spatint.py
@@ -20,12 +20,7 @@ def main() -> None:
     # Scale to mm to make 3D visualisation scaling easier
     sim_data.coords = sim_data.coords*1000.0 # type: ignore
 
-    descriptor = pyvale.SensorDescriptor()
-    descriptor.name = 'Displacement'
-    descriptor.symbol = r'u'
-    descriptor.units = r'm'
-    descriptor.tag = 'DS'
-    descriptor.components = ('x','y','z')
+    descriptor = pyvale.SensorDescriptorFactory.displacement_descriptor()
 
     spat_dims = 2
     field_key = 'disp'
@@ -51,34 +46,18 @@ def main() -> None:
                                               disp_field,
                                               descriptor)
 
-    pos_offset = -1.0*np.ones_like(sensor_positions)
-    pos_offset[:,2] = 0 # in 2d we only have offset in x and y so zero z
-    pos_error_data = pyvale.FieldErrorData(pos_offset_xyz=pos_offset)
+
 
     angle_offset = np.zeros_like(sensor_positions)
-    angle_offset[:,0] = 1.0 # only rotate about z in 2D
-    angle_error_data = pyvale.FieldErrorData(ang_offset_zyx=angle_offset)
+    angle_offset[:,0] = 90.0 # only rotate about z in 2D
 
-    time_offset = 1.0*np.ones_like(disp_sens_array.get_sample_times())
-    time_error_data = pyvale.FieldErrorData(time_offset=time_offset)
-
-
+    field_error_data = pyvale.FieldErrorData(ang_offset_zyx=angle_offset,
+                                             spatial_averager=pyvale.ESpatialIntType.QUAD4PT,
+                                             spatial_dims=np.array([2.0,2.0,0.0]))
 
     field_errs = []
     field_errs.append(pyvale.SysErrField(disp_field,
-                                        time_error_data))
-    field_errs.append(pyvale.SysErrField(disp_field,
-                                        time_error_data))
-
-    field_errs.append(pyvale.SysErrField(disp_field,
-                                        pos_error_data))
-    field_errs.append(pyvale.SysErrField(disp_field,
-                                        pos_error_data))
-
-    field_errs.append(pyvale.SysErrField(disp_field,
-                                        angle_error_data))
-    field_errs.append(pyvale.SysErrField(disp_field,
-                                        angle_error_data))
+                                        field_error_data))
 
     err_int_opts = pyvale.ErrorIntegrationOpts(force_dependence=True,
                                                store_errs_by_func=True)
@@ -90,38 +69,6 @@ def main() -> None:
 
     measurements = disp_sens_array.calc_measurements()
 
-    sens_data_by_chain = error_int.get_sens_data_by_chain()
-    if sens_data_by_chain is not None:
-        for ii,ss in enumerate(sens_data_by_chain):
-            print(80*"-")
-            if ss is not None:
-                print(f"SensorData @ [{ii}]")
-                print("TIME")
-                print(ss.sample_times)
-                print()
-                print("POSITIONS")
-                print(ss.positions)
-                print()
-                print("ANGLES")
-                for aa in ss.angles:
-                    print(aa.as_euler('zyx',degrees=True))
-                print()
-            print(80*"-")
-
-    print()
-    print(80*"=")
-    sens_data_accumulated = error_int.get_sens_data_accumulated()
-    print("TIME")
-    print(sens_data_accumulated.sample_times)
-    print()
-    print("POSITIONS")
-    print(sens_data_accumulated.positions)
-    print()
-    print("ANGLES")
-    for aa in sens_data_accumulated.angles:
-        print(aa.as_euler('zyx',degrees=True))
-    print()
-    print(80*"=")
 
     return
 

src/pyvale/numerical/quadratureintegrator.py
@@ -7,11 +7,11 @@ Copyright (C) 2024 The Digital Validation Team
 '''
 from typing import Callable
 import numpy as np
-from scipy.spatial.transform import Rotation
 from pyvale.physics.field import IField
 from pyvale.numerical.spatialintegrator import (ISpatialIntegrator,
                                                 create_int_pt_array)
 
+from pyvale.sensors.sensordata import SensorData
 
 def create_gauss_weights_2d_4pts(meas_shape: tuple[int,int,int]) -> np.ndarray:
     return np.ones((4,)+meas_shape)
@@ -26,59 +26,50 @@ def create_gauss_weights_2d_9pts(meas_shape: tuple[int,int,int]) -> np.ndarray:
 
 
 class Quadrature2D(ISpatialIntegrator):
-    __slots__ = ("_field","_cent_pos","_area_dims","_sample_times","_area",
-                 "_n_gauss_pts","_gauss_pt_offsets","_gauss_weight_func",
-                 "_gauss_pts","_averages")
+    __slots__ = ("_field","_area","_n_gauss_pts","_gauss_pt_offsets"
+                 ,"_gauss_weight_func","_gauss_pts","_averages","_sens_data")
 
     def __init__(self,
-                 gauss_pt_offsets: np.ndarray,
-                 gauss_weight_func: Callable,
                  field: IField,
-                 cent_pos: np.ndarray,
-                 area_dims: np.ndarray,
-                 sample_times: np.ndarray | None = None) -> None:
+                 sens_data: SensorData,
+                 gauss_pt_offsets: np.ndarray,
+                 gauss_weight_func: Callable) -> None:
 
         self._field = field
-        self._cent_pos = cent_pos
-        self._area_dims = area_dims
-        self._sample_times = sample_times
-
-        self._area = self._area_dims[0]*self._area_dims[1]
+        self._sens_data = sens_data
+        self._area = self._sens_data.spatial_dims[0] * \
+            self._sens_data.spatial_dims[1]
 
         self._n_gauss_pts = gauss_pt_offsets.shape[0]
         self._gauss_pt_offsets = gauss_pt_offsets
         self._gauss_weight_func = gauss_weight_func
 
-        self._gauss_pts = create_int_pt_array(self._gauss_pt_offsets,
-                                              cent_pos)
+        self._gauss_pts = create_int_pt_array(self._sens_data,
+                                              self._gauss_pt_offsets)
         self._averages = None
 
-    def calc_integrals(self,
-                       cent_pos: np.ndarray | None = None,
-                       sample_times: np.ndarray | None = None,
-                       angles: tuple[Rotation,...] | None = None) -> np.ndarray:
-        self._averages = self.calc_averages(cent_pos,sample_times,angles)
+    def calc_integrals(self, sens_data: SensorData | None = None) -> np.ndarray:
+        self._averages = self.calc_averages(sens_data)
         return self._area*self.get_averages()
 
     def get_integrals(self) -> np.ndarray:
         return self._area*self.get_averages()
 
-    def calc_averages(self,
-                      cent_pos: np.ndarray | None = None,
-                      sample_times: np.ndarray | None = None,
-                      angles: tuple[Rotation,...] | None = None) -> np.ndarray:
+    def calc_averages(self, sens_data: SensorData | None = None) -> np.ndarray:
 
-        if cent_pos is not None:
-            # shape=(n_sens*n_gauss_pts,n_dims)
-            self._gauss_pts = create_int_pt_array(self._gauss_pt_offsets,
-                                                  cent_pos)
+        if sens_data is not None:
+            self._sens_data = sens_data
+
+        # shape=(n_sens*n_gauss_pts,n_dims)
+        self._gauss_pts = create_int_pt_array(self._sens_data,
+                                              self._gauss_pt_offsets)
 
         # shape=(n_gauss_pts*n_sens,n_comps,n_timesteps)
         gauss_vals = self._field.sample_field(self._gauss_pts,
-                                              sample_times,
-                                              angles)
+                                              self._sens_data.sample_times,
+                                              self._sens_data.angles)
 
-        meas_shape = (self._cent_pos.shape[0],
+        meas_shape = (self._sens_data.positions.shape[0],
                         gauss_vals.shape[1],
                         gauss_vals.shape[2])
 

src/pyvale/numerical/spatialintegrator.py
@@ -5,25 +5,23 @@ License: MIT
 Copyright (C) 2024 The Digital Validation Team
 ================================================================================
 '''
-import enum
 from abc import ABC, abstractmethod
 import numpy as np
-from scipy.spatial.transform import Rotation
+from pyvale.sensors.sensordata import SensorData
 
 
-class ESpatialIntType(enum.Enum):
-    RECT1PT = enum.auto()
-    RECT4PT = enum.auto()
-    RECT9PT = enum.auto()
-    QUAD4PT = enum.auto()
-    QUAD9PT = enum.auto()
-
-
-def create_int_pt_array(int_pt_offsets: np.ndarray,
-                        cent_pos: np.ndarray,
+def create_int_pt_array(sens_data: SensorData,
+                        int_pt_offsets: np.ndarray,
                         ) -> np.ndarray:
-    offset_array = np.tile(int_pt_offsets,(cent_pos.shape[0],1))
-    int_pt_array = np.repeat(cent_pos,int_pt_offsets.shape[0],axis=0)
+
+    # shape=(n_sens*n_int_pts,n_dims)
+    offset_array = np.tile(int_pt_offsets,(sens_data.positions.shape[0],1))
+
+    if sens_data.angles is not None:
+        for rr in sens_data.angles:
+            offset_array[,:] = np.matmul(rr.as_matrix(),int_pt_offsets.T).T
+
+    int_pt_array = np.repeat(sens_data.positions,int_pt_offsets.shape[0],axis=0)
     # shape=(n_sens*n_int_pts,n_dims)
     return int_pt_array + offset_array
 
@@ -31,9 +29,7 @@ def create_int_pt_array(int_pt_offsets: np.ndarray,
 class ISpatialIntegrator(ABC):
     @abstractmethod
     def calc_averages(self,
-                      cent_pos: np.ndarray | None = None,
-                      sample_times: np.ndarray | None = None,
-                      angles: tuple[Rotation,...] | None = None) -> np.ndarray:
+                      sens_data: SensorData) -> np.ndarray:
         pass
 
     @abstractmethod

src/pyvale/sensors/sensordata.py
@@ -5,13 +5,10 @@ License: MIT
 Copyright (C) 2024 The Digital Validation Team
 ================================================================================
 '''
-import copy
 from dataclasses import dataclass
 import numpy as np
 from scipy.spatial.transform import Rotation
-from pyvale.numerical.spatialintegrator import ESpatialIntType
-from pyvale.physics.field import IField
-
+from pyvale.numerical.spatialinttype import ESpatialIntType
 
 @dataclass(slots=True)
 class SensorData:

src/pyvale/uncertainty/syserrorsfield.py
@@ -13,7 +13,7 @@ from scipy.spatial.transform import Rotation
 from pyvale.physics.field import IField
 from pyvale.physics.fieldsampler import sample_field_with_sensor_data
 from pyvale.sensors.sensordata import SensorData
-from pyvale.numerical.spatialintegrator import ESpatialIntType
+from pyvale.numerical.spatialinttype import ESpatialIntType
 from pyvale.uncertainty.errorcalculator import (IErrCalculator,
                                                 EErrType,
                                                 EErrDependence)
@@ -55,7 +55,6 @@ class SysErrField(IErrCalculator):
         self._err_dep = err_dep
         self._sensor_data_perturbed = SensorData()
 
-
     def get_error_dep(self) -> EErrDependence:
         return self._err_dep
 
@@ -72,7 +71,12 @@ class SysErrField(IErrCalculator):
                   err_basis: np.ndarray,
                   sens_data: SensorData,
                   ) -> tuple[np.ndarray, SensorData]:
+        
         self._sensor_data_perturbed = copy.deepcopy(sens_data)
+        self._sensor_data_perturbed.spatial_averager = \
+            self._field_err_data.spatial_averager
+        self._sensor_data_perturbed.spatial_dims = \
+            self._field_err_data.spatial_dims
 
         self._sensor_data_perturbed.positions = perturb_sensor_positions(
             self._sensor_data_perturbed.positions,
@@ -142,9 +146,6 @@ def perturb_sample_times(sim_time: np.ndarray,
         else:
             return None
 
-    if time_offset is None and time_rand is None and time_drift is None:
-        return None
-
     time_perturbed = np.copy(time_nominal)
 
     if time_offset is not None:
@@ -172,9 +173,6 @@ def perturb_sensor_angles(n_sensors: int,
                 (Rotation.from_euler("zyx",[0,0,0], degrees=True),)
         else:
             return None
-
-    if angle_offsets_zyx is None and rand_ang_zyx is None:
-        return None
 
     angles_perturbed = [Rotation.from_euler("zyx",[0,0,0], degrees=True)] * \
         len(angles_nominal)