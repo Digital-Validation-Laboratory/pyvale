72da8745488b8e4a50d9bf79e097bcdb4c57aeb5
lornasibson <lorna.sibson@ukaea.uk>
2024-11-15 09:27:52 +0000

    script to combine different elements and make scene added

---
+++ dev/lsdev/blenderscene.py
@@ -0,0 +1,35 @@
+import bpy
+
+class BlenderScene:
+    def __init__(self):
+        self.objects = list()
+        self.lights = list()
+        self.cameras = list()
+        bpy.ops.wm.read_factory_settings(use_empty=True) #Blender starts with empty scene, without default objects
+        self.default_settings()
+
+    def default_settings(self):
+        """Sets the default settings in Blender:
+        - Sets the units to mm
+        - Sets the background to black
+        """
+        bpy.context.scene.unit_settings.scale_length = 0.001
+        bpy.context.scene.unit_settings.length_unit = 'MILLIMETERS'
+
+        new_world = bpy.data.worlds.new('World')
+        bpy.context.scene.world = new_world
+        new_world.use_nodes = True
+        node_tree = new_world.node_tree
+        nodes = node_tree.nodes
+
+        nodes.clear()
+        bg_node = nodes.new(type='ShaderNodeBackground')  # Add Background node
+        bg_node.inputs[0].default_value = [0.5, 0.5, 0.5, 1]
+        bg_node.inputs[1].default_value = 0
+
+    def add_light(self):
+
+    def add_camera(self):
+
+    
+
