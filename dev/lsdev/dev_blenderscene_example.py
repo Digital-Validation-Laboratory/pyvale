f97127b32a37ec1fade769ececf8d3fb79bbea72
lornasibson <lorna.sibson@ukaea.uk>
2024-11-25 16:01:41 +0000

    example creates camera now too

--- dev/lsdev/dev_blenderscene_example.py
+++ dev/lsdev/dev_blenderscene_example.py
@@ -2,16 +2,22 @@ from pathlib import Path
 import numpy as np
 import mooseherder as mh
 from blenderscene import BlenderScene
+from dev_partblender import *
 
 def main() -> None:
     data_path = Path('src/data/case13_out.e')
     data_reader = mh.ExodusReader(data_path)
     sim_data = data_reader.read_all_sim_data()
 
-    filepath = '/home/lorna/pyvale/dev/lsdev/trial_blender_part.blend'
+
+    filepath = '/home/lorna/pyvale/dev/lsdev/blender_files/case13.blend'
     scene = BlenderScene()
 
-    scene.add_part(sim_data)
+    part_location = (0, 0, 0)
+    part = scene.add_part(sim_data, location=part_location)
+
+
+    camera = scene.add_camera()
 
     scene.save_model(filepath)
 
