dcc1020bbce9a36f7feff031c40c01f96cce7ee1
lornasibson <lorna.sibson@ukaea.uk>
2024-11-26 09:25:17 +0000

    example changed to set part location and add light

--- dev/lsdev/dev_blenderscene_example.py
+++ dev/lsdev/dev_blenderscene_example.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import numpy as np
 import mooseherder as mh
@@ -5,19 +6,31 @@ from blenderscene import BlenderScene
 from dev_partblender import *
 
 def main() -> None:
+    # cwd = Path.cwd()
+    # data_path = cwd.parents[0] / 'mooseherder/scripts/moose/moose-mech-simple_out.e'
     data_path = Path('src/data/case13_out.e')
     data_reader = mh.ExodusReader(data_path)
     sim_data = data_reader.read_all_sim_data()
 
+    dir = Path.cwd() / 'dev/lsdev/blender_files'
+    filename = 'case13.blend'
+    filepath = dir / filename
+    all_files = os.listdir(dir)
+    for ff in all_files:
+        if filename == ff:
+            os.remove(dir / ff)
 
-    filepath = '/home/lorna/pyvale/dev/lsdev/blender_files/case13.blend'
+    filepath = str(filepath)
+
     scene = BlenderScene()
 
     part_location = (0, 0, 0)
-    part = scene.add_part(sim_data, location=part_location)
-
+    part = scene.add_part(sim_data)
+    scene.set_part_location(part, part_location)
 
     camera = scene.add_camera()
+
+    light = scene.add_light()
 
     scene.save_model(filepath)
 
