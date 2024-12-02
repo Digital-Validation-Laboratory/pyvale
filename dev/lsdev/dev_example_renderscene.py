d93af586e90025751a547373187734ac37852226
lornasibson <lorna.sibson@ukaea.uk>
2024-12-02 15:54:36 +0000

    example to make scene and render image

---
+++ dev/lsdev/dev_example_renderscene.py
@@ -0,0 +1,66 @@
+import os
+from pathlib import Path
+import mooseherder as mh
+from blenderscene import BlenderScene
+from dev_partblender import *
+from camera import CameraData
+from lightingblender import LightData, LightType
+from render import RenderData, Render
+
+def main() -> None:
+    # Making Blender scene
+    data_path = Path('src/data/case13_out.e')
+    data_reader = mh.ExodusReader(data_path)
+    sim_data = data_reader.read_all_sim_data()
+
+    dir = Path.cwd() / 'dev/lsdev/blender_files'
+    filename = 'case13.blend'
+    filepath = dir / filename
+    all_files = os.listdir(dir)
+    for ff in all_files:
+        if filename == ff:
+            os.remove(dir / ff)
+
+    filepath = str(filepath)
+
+    scene = BlenderScene()
+
+    part_location = (0, 0, 0)
+    part = scene.add_part(sim_data)
+    scene.set_part_location(part, part_location)
+
+    sensor_px = (2452, 2056)
+    cam_position = (0, 0, 200)
+    focal_length = 15.0
+    cam_data = CameraData(sensor_px=sensor_px,
+                          position=cam_position,
+                          focal_length=focal_length)
+
+    camera = scene.add_camera(cam_data)
+
+    type = LightType.POINT
+    light_position = (0, 0, 200)
+    energy = 20 * (10)**6
+    light_data = LightData(type=type,
+                           position=light_position,
+                           energy=energy)
+
+    light = scene.add_light(light_data)
+
+    #---------------------------------------------------------------------------
+    # Rendering images
+    image_path = Path.cwd() / 'dev/lsdev/rendered_images'
+    output_path = Path.cwd() / 'dev/lsdev/rendered_images'
+
+
+    render_data = RenderData(samples=1)
+    render = Render(render_data, image_path=image_path, output_path=output_path)
+
+    render_counter = 0
+
+    for i in range(render_data.samples):
+        render.render_image(render_counter)
+        render_counter += 1
+
+if __name__ == "__main__":
+    main()
