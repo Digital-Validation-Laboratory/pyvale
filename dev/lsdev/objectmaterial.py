2cb6024da4a74ec545a5c731afd5aa5dc418bd99
lornasibson <lorna.sibson@ukaea.uk>
2024-12-04 09:59:48 +0000

    uv unwrap added so image texture works

--- dev/lsdev/objectmaterial.py
+++ dev/lsdev/objectmaterial.py
@@ -1,3 +1,4 @@
+import os
 from dataclasses import dataclass
 import bpy
 
@@ -5,6 +6,7 @@ import bpy
 class MaterialData():
     roughness: float | None = 0.5
     metallic: float | None = 0
+    interpolant = 'Cubic'
     # TODO: add other material properties to here
 
 class BlenderMaterial():
@@ -12,24 +14,63 @@ class BlenderMaterial():
         self.mat_data = MaterialData
         self.object = object
         self.image_path = image_path
+        self.mat = None
+        self.tree = None
+        self.nodes = None
+
+    def _uv_unwrap(self):
+        self.object.select_set(True)
+        bpy.context.view_layer.objects.active = self.object
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action='SELECT')
+        bpy.ops.uv.smart_project()
+        bpy.ops.object.mode_set(mode='OBJECT')
+        self.object.select_set(False)
 
     def _clear_nodes(self):
         self.object.select_set(True)
-        mat = bpy.data.materials.new(name='Material') # add this to init?
-        mat.use_nodes = True
-        tree = mat.node_tree
-        nodes = tree.nodes
-        nodes.clear()
+        self.mat = bpy.data.materials.new(name='Material') # add this to init?
+        self.mat.use_nodes = True
+        self.object.active_material = self.mat
+        self.tree = self.mat.node_tree
+        self.nodes = self.tree.nodes
+        self.nodes.clear()
 
-    def _set_basic_material(self):
-        bsdf = nodes.new(type='ShaderNodeBsdfPrincipled')
+    def _set_image_texture(self):
+        bsdf = self.nodes.new(type='ShaderNodeBsdfPrincipled')
         bsdf.location = (0, 0)
         bsdf.inputs['Roughness'].default_value = self.mat_data.roughness
         bsdf.inputs['Metallic'].default_value = self.mat_data.metallic
 
-    def _set_image_texture(self):
-        tex_image = nodes.new(type='ShaderNodeTexImage')
+        tex_image = self.nodes.new(type='ShaderNodeTexImage')
         tex_image.location = (0, 0)
+
+        if os.path.exists(self.image_path):
+            tex_image.image = bpy.data.images.load(self.image_path)
+        else:
+            print('Failed to load image')
+
+        tex_image.interpolation = self.mat_data.interpolant
+
+        output = self.nodes.new(type='ShaderNodeOutputMaterial')
+        output.location = (0, 0)
+
+        self.tree.links.new(tex_image.outputs['Color'], bsdf.inputs['Base Color'])
+        self.tree.links.new(bsdf.outputs['BSDF'], output.inputs['Surface'])
+
+        obj = bpy.data.objects.get('part')
+        if obj:
+            obj.active_material = self.mat
+
+    def add_material(self):
+        self._uv_unwrap()
+        self._clear_nodes()
+        self._set_image_texture()
+
+        return self.mat
+
+
+
 
 
 
