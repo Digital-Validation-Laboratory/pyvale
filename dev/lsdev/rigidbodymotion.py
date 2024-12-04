44553eb1cc1a5a4602158dcc1168ea1db3ac6a0d
lornasibson <lorna.sibson@ukaea.uk>
2024-12-04 16:10:06 +0000

    file naming corrected

--- dev/lsdev/rigidbodymotion.py
+++ dev/lsdev/rigidbodymotion.py
@@ -9,44 +9,45 @@ class RigidBodyMotion:
         self.image_path = image_path
         self.output_path = output_path
 
-    def rigid_body_z(self, z_lims):
+    def rigid_body_motion_z(self, z_lims):
         min_z = z_lims[0]
         max_z = z_lims[1]
 
-        n_steps = max_z - min_z
+        n_steps = (max_z - min_z) / self.step
+        render_counter = 0
 
-        for z in range(n_steps, self.step):
-            z_location = z + min_z
+        # TODO: Make this loop work with int + float values
+
+        for z in range(n_steps):
+            z_location = (z * self.step) + min_z
+            print(f"{z_location=}")
             self.part.location = (self.part_location[0],
                                   self.part_location[1],
                                   (self.part_location[2] + z_location))
             render_data = RenderData(samples=1)
             render = Render(render_data, self.image_path, self.output_path)
 
-            render_counter = 0
             render_name = 'rigid_body_motion_z'
-
-            for i in range(render_data.samples):
-                render.render_image(render_name, render_counter)
-                render_counter += 1
+            render.render_image(render_name, render_counter)
+            render_counter += 1
 
     def rigid_body_motion_x(self, x_lims):
         min_x = x_lims[0]
         max_x = x_lims[1]
 
-        n_steps = max_x - min_x
+        n_steps = int((max_x - min_x) / self.step)
+        render_counter = 0
 
-        for x in range(n_steps, self.step):
-            x_location = x + min_x
+        for x in range(n_steps):
+            x_location = (x * self.step) + min_x
+            print(f"{x_location=}")
             self.part.location = ((self.part_location[0] + x_location),
                                   self.part_location[1],
                                   self.part_location[2])
             render_data = RenderData(samples=1)
             render = Render(render_data, self.image_path, self.output_path)
 
-            render_counter = 0
             render_name = 'rigid_body_motion_x'
 
-            for i in range(render_data.samples):
-                render.render_image(render_name, render_counter)
-                render_counter += 1
+            render.render_image(render_name, render_counter)
+            render_counter += 1
