2ce270da1c52ae6ca0175e00e4414c38e4c5fd96
lornasibson <lorna.sibson@ukaea.uk>
2024-12-06 09:56:27 +0000

    type hinting added

--- dev/lsdev/dev_blenderscene.py
+++ dev/lsdev/dev_blenderscene.py
@@ -1,20 +1,20 @@
 import bpy
-from dev.lsdev.dev_blendercamera import CameraData, CameraBlender
-from dev.lsdev.dev_lightingblender import LightData, BlenderLight
+from mooseherder import SimData
+from dev_blendercamera import CameraData, CameraBlender
+from dev_lightingblender import LightData, BlenderLight
 from dev_partblender import BlenderPart
-from dev.lsdev.dev_objectmaterial import BlenderMaterial
+from dev_objectmaterial import MaterialData, BlenderMaterial
 
 class BlenderScene:
     def __init__(self):
-        self.objects = list()
-        self.lights = list()
-        self.cameras = list()
         bpy.ops.wm.read_factory_settings(use_empty=True) #Blender starts with empty scene, without default objects
         self.default_settings()
 
     def default_settings(self):
         """Sets the default settings in Blender:
         - Sets the units to mm
+
+        # part = partmaker.add_thickness(part=part)
         - Sets the background to black
         """
         bpy.context.scene.unit_settings.scale_length = 0.001
@@ -27,7 +27,7 @@ class BlenderScene:
         nodes = node_tree.nodes
 
         nodes.clear()
-        bg_node = nodes.new(type='ShaderNodeBackground')  # Add Background node
+        bg_node = nodes.new(type='ShaderNodeBackground')
         bg_node.inputs[0].default_value = [0.5, 0.5, 0.5, 1]
         bg_node.inputs[1].default_value = 0
 
@@ -36,30 +36,28 @@ class BlenderScene:
         light = lightmaker.add_light()
         return light
 
-    def add_camera(self, cam_data):
+    def add_camera(self, cam_data: CameraData):
         cameramaker = CameraBlender(cam_data)
         camera = cameramaker.add_camera()
 
         return camera
 
-    def add_part(self, sim_data):
+    def add_part(self, sim_data: SimData):
         partmaker = BlenderPart(sim_data)
         part = partmaker.simdata_to_part()
-
-        # part = partmaker.add_thickness(part=part)
 
         self._set_origin(part)
 
         return part
 
-    def set_part_location(self, part, location):
+    def set_part_location(self, part, location: tuple):
         part.location = location
 
-    def set_part_roation(self, part, rotation):
+    def set_part_roation(self, part, rotation: tuple):
         part.rotation_mode = 'XYZ'
         part.rotation_euler = rotation
 
-    def add_material(self, mat_data, part, image_path):
+    def add_material(self, mat_data: MaterialData, part, image_path: str):
         materialmaker = BlenderMaterial(mat_data, part, image_path)
         mat = materialmaker.add_material()
 
@@ -71,7 +69,7 @@ class BlenderScene:
         part.select_set(True)
         bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
 
-    def save_model(self, filepath):
+    def save_model(self, filepath: str):
         '''
         Method to save the blender model to a file
         '''
