e7ec2d10c8fa11f2163033d99fa5deb036e01dbb
lornasibson <lorna.sibson@ukaea.uk>
2024-12-09 11:18:00 +0000

    printing object location for every render

--- dev/lsdev/dev_rigidbodymotion.py
+++ dev/lsdev/dev_rigidbodymotion.py
@@ -1,13 +1,15 @@
 import bpy
 from dev_render import RenderData, Render
+from dev_blendercamera import CameraData
 
 class RigidBodyMotion:
-    def __init__(self, part, step, part_location: tuple, image_path: str, output_path: str):
+    def __init__(self, part, step, part_location: tuple, image_path: str, output_path: str, cam_data: CameraData):
         self.part = part
         self.step = step
         self.part_location = part_location
         self.image_path = image_path
         self.output_path = output_path
+        self.cam_data = cam_data
 
     def rigid_body_motion_z(self, z_lims: tuple):
         min_z = z_lims[0]
@@ -23,9 +25,10 @@ class RigidBodyMotion:
                                   self.part_location[1],
                                   (self.part_location[2] + z_location))
             render_data = RenderData(samples=1)
-            render = Render(render_data, self.image_path, self.output_path)
+            render = Render(render_data, self.image_path, self.output_path, self.cam_data)
 
             render_name = 'rigid_body_motion_z'
+            print(f"{self.part.location=}")
             render.render_image(render_name, render_counter)
             render_counter += 1
 
@@ -43,9 +46,11 @@ class RigidBodyMotion:
                                   self.part_location[1],
                                   self.part_location[2])
             render_data = RenderData(samples=1)
-            render = Render(render_data, self.image_path, self.output_path)
+            render = Render(render_data, self.image_path, self.output_path, self.cam_data)
 
             render_name = 'rigid_body_motion_x'
+
+            print(f"{self.part.location=}")
 
             render.render_image(render_name, render_counter)
             render_counter += 1
