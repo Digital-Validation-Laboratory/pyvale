c365400cbdce9386ac3fc0e44eda6317cb7dea03
lornasibson <lorna.sibson@ukaea.uk>
2024-12-16 16:05:42 +0000

    class to deform mesh and then deform part with deformed mesh

--- dev/lsdev/dev_deform_part.py
+++ dev/lsdev/dev_deform_part.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pathlib import Path
+import bpy
 import mooseherder as mh
 from mooseherder import SimData
 from dev_partblender import BlenderPart
@@ -23,9 +24,53 @@ class DeformMesh:
 
         coords_new = np.einsum('ij,nj->ni', defgrad_inv, centred)
 
-        coords_uncentred = coords_new + mid
+        return coords_new
 
-        return coords_uncentred
+class DeformPart:
+    def __init__(self, part, deformed_nodes):
+        self.part = part
+        self.deformed_nodes = deformed_nodes * 1000
+
+    def set_new_frame(self):
+        frame_incr = 20
+        ob = bpy.context.view_layer.objects.active
+        if ob is None:
+            bpy.context.objects.active = self.part
+
+        current_frame = bpy.context.scene.frame_current
+        current_frame += frame_incr
+        bpy.context.scene.frame_set(current_frame)
+
+        bpy.data.shape_keys['Key'].eval_time = current_frame
+        self.part.data.shape_keys.keyframe_insert('eval_time', frame=current_frame)
+        bpy.context.scene.frame_end = current_frame
+
+    def deform_part(self):
+        if self.part.data.shape_keys is None:
+            self.part.shape_key_add()
+            self.set_new_frame()
+        sk = self.part.shape_key_add()
+        self.part.data.shape_keys.use_relative = False
+
+        n_nodes_layer = int(len(self.part.data.vertices))
+        all_nodes = np.array([sk.data[i].co for i in range(len(self.part.data.vertices))])
+        first_layer = all_nodes[0:n_nodes_layer, :]
+
+        count = 0
+        for i in range(len(self.part.data.vertices)):
+            if i < n_nodes_layer:
+                sk.data[i].co = self.deformed_nodes[i]
+                count += 1
+            else:
+                dist = np.linalg.norm(first_layer - sk.data[i].co, axis=1)
+                cn = np.argmin(dist)
+                sk.data[i].co = self.deformed_nodes[cn]
+
+        return self.part
+
+
+
+
 
 
 
