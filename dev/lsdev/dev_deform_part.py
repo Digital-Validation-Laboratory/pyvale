58193364a42e2b79d776c315c7d0f9a69574bdc2
lornasibson <lorna.sibson@ukaea.uk>
2025-01-06 17:10:38 +0000

    surface mesh made and printed

--- dev/lsdev/dev_deform_part.py
+++ dev/lsdev/dev_deform_part.py
@@ -78,6 +78,7 @@ class DeformSimData:
                                                                 spat_dim=3)
         pv_surf = pv_grid.extract_surface()
         surface_points = pv_surf.points
+        print(f"{surface_points=}")
 
         return surface_points
 
