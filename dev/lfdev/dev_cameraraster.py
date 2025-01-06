Fixed bug with zooming in and masking the field vertex attributes.

dev/lfdev/dev_cameraraster.py
@@ -85,7 +85,7 @@ def main() -> None:
     connect = sim_data.connect["connect1"]
     field_scalar = sim_data.node_vars["temperature"]
 
-    rot_axis: str = "x"
+    rot_axis: str = "n"
     phi_y_degs: float = 45
     theta_x_degs: float = 45
 
@@ -93,7 +93,7 @@ def main() -> None:
     theta_x_rads: float = theta_x_degs * np.pi/180.0
 
     # Set this to 250 to zoom in
-    image_dist: float = 600.0
+    image_dist: float = 200.0
     #image_dist: float = 250.0
 
     # Set this to 0.0 to get some of the plate outside the FOV
@@ -120,12 +120,12 @@ def main() -> None:
                                   image_dist])
         cam_rot = Rotation.from_euler("zyx", [0, 0, 0], degrees=True)
 
-    dist_cam_to_roi = np.linalg.norm(cam_pos_world - roi_pos_world)
+    image_dist = np.linalg.norm(cam_pos_world - roi_pos_world)
 
     print()
     print(80*"-")
     print(f"{cam_pos_world=}")
-    print(f"{dist_cam_to_roi=}")
+    print(f"{image_dist=}")
     print(80*"-")
 
     cam_to_world_mat = np.zeros((4,4))
@@ -157,7 +157,7 @@ def main() -> None:
     sensor_size = cam_num_px*pixel_size
 
     image_dims_from_res = resolution*cam_num_px
-    image_dims = dist_cam_to_roi * sensor_size / focal_leng
+    image_dims = image_dist * sensor_size / focal_leng
     res_check = image_dims / cam_num_px
 
     left = -image_dims[0]/2
@@ -256,14 +256,24 @@ def main() -> None:
     elem_raster_coord_max = elem_raster_coord_max[:,mask]
     elem_raster_coords = elem_raster_coords[:,:,mask]
     num_elems_in_scene = elem_raster_coords.shape[2]
+    # shape=(nodes_per_elem,elems_in_scene,num_time_steps)
+    field_divide_z = field_divide_z[:,mask,:]
+
     print()
     print(80*"-")
+    print("MASKING CHECKS:")
     print("Mask =")
     print(mask)
     print()
     print(f"Elems in mask =      {np.sum(np.sum(mask))}")
     print(f"Total elems =        {num_elems}")
     print(f"Num elems in scene = {num_elems_in_scene}")
+    print()
+    print(f"{elem_raster_coords.shape=}")
+    print(f"{elem_raster_coord_min.shape=}")
+    print(f"{elem_raster_coord_max.shape=}")
+    print()
+    print(f"{field_divide_z.shape=}")
     print(80*"-")
 
     # Pixel coords
@@ -385,9 +395,9 @@ def main() -> None:
                       + vert_2[zz] * interp_weights[2,:])
 
         field_interp = ((field_frame_divide_z[0,ee] * interp_weights[0,:]
-                    + field_frame_divide_z[1,ee] * interp_weights[1,:]
-                    + field_frame_divide_z[2,ee] * interp_weights[2,:])
-                    * px_coord_z)
+                       + field_frame_divide_z[1,ee] * interp_weights[1,:]
+                       + field_frame_divide_z[2,ee] * interp_weights[2,:])
+                       * px_coord_z)
 
         # Get the pixel indices that are inside the element
         px_inds_x_inside = px_inds_grid_x[edge_mask_grid]
@@ -395,7 +405,8 @@ def main() -> None:
 
         # Build a mask to replace the depth information if there is already an
         # element in front of the one we are rendering
-        px_coord_z_depth_mask = px_coord_z < depth_buffer[px_inds_y_inside,px_inds_x_inside]
+        px_coord_z_depth_mask = (px_coord_z
+                                 < depth_buffer[px_inds_y_inside,px_inds_x_inside])
 
         # Initialise the z coord to the value in the depth buffer
         px_coord_z_masked = depth_buffer[px_inds_y_inside,px_inds_x_inside]
@@ -419,8 +430,11 @@ def main() -> None:
     print("RASTER LOOP END")
     print(80*"=")
 
-    im_to_plot = image_buffer
     plot_on = True
+    depth_to_plot = np.copy(depth_buffer)
+    depth_to_plot[depth_buffer > 10*image_dist] = np.NaN
+    image_to_plot = np.copy(image_buffer)
+    image_to_plot[depth_buffer > 10*image_dist] = np.NaN
     #===========================================================================
     if plot_on:
         plot_opts = pyvale.PlotOptsGeneral()
@@ -428,7 +442,21 @@ def main() -> None:
         (fig, ax) = plt.subplots(figsize=plot_opts.single_fig_size_square,
                                 layout='constrained')
         fig.set_dpi(plot_opts.resolution)
-        cset = plt.imshow(im_to_plot,
+        cset = plt.imshow(depth_to_plot,
+                        cmap=plt.get_cmap(plot_opts.cmap_seq))
+                        #origin='lower')
+        ax.set_aspect('equal','box')
+        fig.colorbar(cset)
+        ax.set_title("Depth buffer",fontsize=plot_opts.font_head_size)
+        ax.set_xlabel(r"x ($px$)",
+                    fontsize=plot_opts.font_ax_size, fontname=plot_opts.font_name)
+        ax.set_ylabel(r"y ($px$)",
+                    fontsize=plot_opts.font_ax_size, fontname=plot_opts.font_name)
+
+        (fig, ax) = plt.subplots(figsize=plot_opts.single_fig_size_square,
+                                layout='constrained')
+        fig.set_dpi(plot_opts.resolution)
+        cset = plt.imshow(image_to_plot,
                         cmap=plt.get_cmap(plot_opts.cmap_seq))
                         #origin='lower')
         ax.set_aspect('equal','box')
@@ -438,34 +466,6 @@ def main() -> None:
                     fontsize=plot_opts.font_ax_size, fontname=plot_opts.font_name)
         ax.set_ylabel(r"y ($px$)",
                     fontsize=plot_opts.font_ax_size, fontname=plot_opts.font_name)
-
-        (fig, ax) = plt.subplots(figsize=plot_opts.single_fig_size_square,
-                                layout='constrained')
-        fig.set_dpi(plot_opts.resolution)
-        cset = plt.imshow(im_to_plot,
-                        cmap=plt.get_cmap(plot_opts.cmap_seq),
-                        origin='lower')
-        ax.set_aspect('equal','box')
-        fig.colorbar(cset)
-        ax.set_title("Origin lower",fontsize=plot_opts.font_head_size)
-        ax.set_xlabel(r"x ($px$)",
-                    fontsize=plot_opts.font_ax_size, fontname=plot_opts.font_name)
-        ax.set_ylabel(r"y ($px$)",
-                    fontsize=plot_opts.font_ax_size, fontname=plot_opts.font_name)
-
-        # camera = pyvale.CameraBasic2D(cam_data=cam_data,
-        #                             field=t_field,
-        #                             descriptor=descriptor)
-
-        # measurements = camera.calc_measurements()
-        # meas_images = camera.get_measurement_images()
-
-        # print(80*"=")
-        # print(f"{measurements.shape=}")
-        # print(f"{meas_images.shape=}")
-        # print(80*"=")
-
-        # (fig,ax) = pyvale.plot_measurement_image(camera,field_key)
 
         plt.show()
 

dev/lfdev/dev_cameraraster_antialias.py
@@ -94,8 +94,7 @@ def main() -> None:
     print(80*"-")
     print()
 
-
-    rot_axis: str = "n"
+    rot_axis: str = "y"
     phi_y_degs: float = 45
     theta_x_degs: float = 45
 
@@ -118,7 +117,7 @@ def main() -> None:
     else:
         cam_num_px = np.array([510,260],dtype=np.int32)
         pixel_size = np.array([10.0e-3,10.0e-3])
-        image_dist: float = 500.0
+        image_dist: float = 300.0
         focal_leng = 25.0
 
     if rot_axis == "y":
@@ -141,14 +140,14 @@ def main() -> None:
                                   image_dist])
         cam_rot = Rotation.from_euler("zyx", [0, 0, 0], degrees=True)
 
-    dist_cam_to_roi = np.linalg.norm(cam_pos_world - roi_pos_world)
+    image_dist = np.linalg.norm(cam_pos_world - roi_pos_world)
 
     time_start_setup = time.perf_counter()
 
     print()
     print(80*"-")
     print(f"{cam_pos_world=}")
-    print(f"{dist_cam_to_roi=}")
+    print(f"{image_dist=}")
     print(80*"-")
 
     cam_to_world_mat = np.zeros((4,4))
@@ -173,8 +172,7 @@ def main() -> None:
     print(80*"-")
 
     sensor_size = cam_num_px*pixel_size
-    image_dims = dist_cam_to_roi * sensor_size / focal_leng
-    resolution = image_dims / cam_num_px
+    image_dims = image_dist*sensor_size/focal_leng
     #///////////////////////////////////////////////////////////////////////////
 
     #---------------------------------------------------------------------------
@@ -251,14 +249,24 @@ def main() -> None:
     elem_raster_coord_max = elem_raster_coord_max[:,mask]
     elem_raster_coords = elem_raster_coords[:,:,mask]
     num_elems_in_scene = elem_raster_coords.shape[2]
+    # shape=(nodes_per_elem,elems_in_scene,num_time_steps)
+    field_divide_z = field_divide_z[:,mask,:]
+
     print()
     print(80*"-")
+    print("MASKING CHECKS:")
     print("Mask =")
     print(mask)
     print()
     print(f"Elems in mask =      {np.sum(np.sum(mask))}")
     print(f"Total elems =        {num_elems}")
     print(f"Num elems in scene = {num_elems_in_scene}")
+    print()
+    print(f"{elem_raster_coords.shape=}")
+    print(f"{elem_raster_coord_min.shape=}")
+    print(f"{elem_raster_coord_max.shape=}")
+    print()
+    print(f"{field_divide_z.shape=}")
     print(80*"-")
 
     # Find the indices of the bounding box that each element lies within on the
@@ -437,8 +445,10 @@ def main() -> None:
     print()
 
     plot_on = True
-    depth_to_plot = depth_avg
+    depth_to_plot = np.copy(depth_avg)
     depth_to_plot[depth_avg > 10*image_dist] = np.NaN
+    image_to_plot = np.copy(image_avg)
+    image_to_plot[depth_avg > 10*image_dist] = np.NaN
     #===========================================================================
     if plot_on:
         plot_opts = pyvale.PlotOptsGeneral()
@@ -460,7 +470,7 @@ def main() -> None:
         (fig, ax) = plt.subplots(figsize=plot_opts.single_fig_size_square,
                                 layout='constrained')
         fig.set_dpi(plot_opts.resolution)
-        cset = plt.imshow(image_avg,
+        cset = plt.imshow(image_to_plot,
                         cmap=plt.get_cmap(plot_opts.cmap_seq))
                         #origin='lower')
         ax.set_aspect('equal','box')

dev/lfdev/dev_cameraraster_surf3d.py
@@ -230,13 +230,13 @@ def main() -> None:
     # Number of divisions (subsamples) for each pixel for anti-aliasing
     sub_samp: int = 1
 
-    cam_type = "Test"
+    cam_type = "AV507"
     if cam_type == "AV507":
         cam_num_px = np.array([2464,2056],dtype=np.int32)
         pixel_size = np.array([3.45e-3,3.45e-3]) # in millimeters!
         focal_leng = 25.0
 
-        imaging_rad: float = 300.0 # Not needed for camera data, just for cam pos below
+        imaging_rad: float = 100.0 # Not needed for camera data, just for cam pos below
     else:
         cam_num_px = np.array([510,260],dtype=np.int32)
         pixel_size = np.array([10.0e-3,10.0e-3])
@@ -270,7 +270,7 @@ def main() -> None:
                                 rot_world=cam_rot,
                                 roi_center_world=roi_pos_world,
                                 focal_length=focal_leng,
-                                sub_samp=1)
+                                sub_samp=sub_samp)
 
     print()
     print(80*"-")
@@ -304,29 +304,26 @@ def main() -> None:
     #---------------------------------------------------------------------------
     # BACK FACE CULLING
     num_elems = connect.shape[1]
-    coords_cam = np.matmul(cam_data.world_to_cam_mat,coords_world_with_w)
+    # coords_cam = np.matmul(cam_data.world_to_cam_mat,coords_world_with_w)
 
-    # shape=(coord[X,Y,Z,W],node_per_elem,elem_num)
-    elem_cam_coords = coords_cam[:,connect]
-    # shape=(nodes_per_elem,coord[X,Y,Z,W],elem_num)
-    elem_cam_coords = np.swapaxes(elem_cam_coords,0,1)
+    # # shape=(coord[X,Y,Z,W],node_per_elem,elem_num)
+    # elem_cam_coords = coords_cam[:,connect]
+    # # shape=(nodes_per_elem,coord[X,Y,Z,W],elem_num)
+    # elem_cam_coords = np.swapaxes(elem_cam_coords,0,1)
 
-    # Calculate the normal vectors for all of the elements
-    elem_cam_edge0 = elem_cam_coords[1,:-1,:] - elem_cam_coords[0,:-1,:]
-    elem_cam_edge1 = elem_cam_coords[2,:-1,:] - elem_cam_coords[0,:-1,:]
-    elem_cam_normals = np.cross(elem_cam_edge0,elem_cam_edge1,
-                                axisa=0,axisb=0).T
-    # Normalise to unit vectors
-    elem_cam_normals = elem_cam_normals / np.linalg.norm(elem_cam_normals,axis=0)
+    # # Calculate the normal vectors for all of the elements
+    # elem_cam_edge0 = elem_cam_coords[1,:-1,:] - elem_cam_coords[0,:-1,:]
+    # elem_cam_edge1 = elem_cam_coords[2,:-1,:] - elem_cam_coords[0,:-1,:]
+    # elem_cam_normals = np.cross(elem_cam_edge0,elem_cam_edge1,
+    #                             axisa=0,axisb=0).T
+    # # Normalise to unit vectors
+    # elem_cam_normals = elem_cam_normals / np.linalg.norm(elem_cam_normals,axis=0)
 
-    print()
-    print(elem_cam_edge0.shape)
-    print(elem_cam_edge1.shape)
-    print(elem_cam_normals.shape)
-    print()
-
-    return
-
+    # print()
+    # print(elem_cam_edge0.shape)
+    # print(elem_cam_edge1.shape)
+    # print(elem_cam_normals.shape)
+    # print()
 
     # TODO
     # Pass the masked coordinates in camera world to the world_to_raster function
@@ -337,8 +334,6 @@ def main() -> None:
 
     # Convert to perspective correct hyperbolic interpolation for z interp
     coords_raster[zz,:] = 1/coords_raster[zz,:]
-
-
 
     # shape=(coord[X,Y,Z],node_per_elem,elem_num)
     elem_raster_coords = coords_raster[:,connect]
@@ -371,14 +366,24 @@ def main() -> None:
     elem_raster_coord_max = elem_raster_coord_max[:,mask]
     elem_raster_coords = elem_raster_coords[:,:,mask]
     num_elems_in_scene = elem_raster_coords.shape[2]
+    # shape=(nodes_per_elem,elems_in_scene,num_time_steps)
+    field_divide_z = field_divide_z[:,mask,:]
+
     print()
     print(80*"-")
+    print("MASKING CHECKS:")
     print("Mask =")
     print(mask)
     print()
     print(f"Elems in mask =      {np.sum(np.sum(mask))}")
     print(f"Total elems =        {num_elems}")
     print(f"Num elems in scene = {num_elems_in_scene}")
+    print()
+    print(f"{elem_raster_coords.shape=}")
+    print(f"{elem_raster_coord_min.shape=}")
+    print(f"{elem_raster_coord_max.shape=}")
+    print()
+    print(f"{field_divide_z.shape=}")
     print(80*"-")
 
     # Find the indices of the bounding box that each element lies within on the
@@ -560,8 +565,10 @@ def main() -> None:
     print()
 
     plot_on = True
-    depth_to_plot = depth_avg
+    depth_to_plot = np.copy(depth_avg)
     depth_to_plot[depth_avg > 10*cam_data.image_dist] = np.NaN
+    image_to_plot = np.copy(image_avg)
+    image_to_plot[depth_avg > 10*cam_data.image_dist] = np.NaN
     #===========================================================================
     if plot_on:
         plot_opts = pyvale.PlotOptsGeneral()
@@ -583,12 +590,12 @@ def main() -> None:
         (fig, ax) = plt.subplots(figsize=plot_opts.single_fig_size_square,
                                 layout='constrained')
         fig.set_dpi(plot_opts.resolution)
-        cset = plt.imshow(image_avg,
+        cset = plt.imshow(image_to_plot,
                         cmap=plt.get_cmap(plot_opts.cmap_seq))
                         #origin='lower')
         ax.set_aspect('equal','box')
         fig.colorbar(cset)
-        ax.set_title("Image",fontsize=plot_opts.font_head_size)
+        ax.set_title("Field Image",fontsize=plot_opts.font_head_size)
         ax.set_xlabel(r"x ($px$)",
                     fontsize=plot_opts.font_ax_size, fontname=plot_opts.font_name)
         ax.set_ylabel(r"y ($px$)",