3212d695dd166caef0674057604c6244b651de02
lornasibson <lorna.sibson@ukaea.uk>
2025-01-14 15:09:51 +0000

    writing progress to text file added

--- dev/lsdev/dev_render.py
+++ dev/lsdev/dev_render.py
@@ -19,7 +19,11 @@ class RenderData:
 
 
 class Render:
-    def __init__(self, RenderData, image_path: str, output_path: str, cam_data: CameraData):
+    def __init__(self,
+                 RenderData,
+                 image_path: str,
+                 output_path: str,
+                 cam_data: CameraData):
         self.render_data = RenderData
         self.image_path = image_path
         self.output_path = output_path
@@ -37,16 +41,30 @@ class Render:
         self.scene.render.filepath =  str(self.image_path / file_name)
         self.scene.render.threads_mode = 'FIXED'
         self.scene.render.threads = cores
+        self.scene.cycles.use_denoising = False # To make rendering faster
 
         bpy.context.scene.render.image_settings.file_format = 'TIFF'
 
         bpy.ops.render.render(write_still=True)
 
-    def render_image(self, name: int, image_count: int):
+    def render_image(self, name: int, image_count: int, part):
 
         file_name = name + '_' + str(image_count) + '.tiff'
         cores = int(cpu_count())
         self.render_parameters(file_name, cores)
-        # TODO: Write progress to output path
+        self._write_progress(image_count, part)
+
+    def _write_progress(self, image_count: int, part):
+        if image_count == 0:
+            report = open(self.output_path, 'w', encoding='utf-8')
+        else:
+            report = open(self.output_path, 'a', encoding='utf-8')
+        report.write('\nOn render: ' + str(image_count))
+        report.write('\nPart dimensions: ' + str(part.dimensions))
+        report.write('\nPart location: ' + str(part.location))
+        report.write('\nPart rotation: ' + str(part.rotation_euler))
+        report.write('\n')
+        report.close()
+
 
 
